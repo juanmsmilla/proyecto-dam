fc0aec1c946aa1efa0ac16311edec22ed0fb4ec2
Juan Miguel Sánchez Milla <juanmsmilla@gmail.com>
2023-05-31 07:15:55 +0200

    first attempt insert chat

--- backend/core.py
+++ backend/core.py
@@ -12,16 +12,32 @@ def indexing_pdf(pdf, chunk_size: int = 1000, chunk_overlap: int = 200):
         text += page.extract_text()
 
     # chunks
-    text_splitter = CharacterTextSplitter(
+    splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap,
         length_function=len
         )
-    chunks = text_splitter.split_text(text)
+    chunks = splitter.split_text(text)
 
     # FAISS
     embeddings = OpenAIEmbeddings()
     vector_index = FAISS.from_texts(chunks, embeddings)
 
     return vector_index
+
+
+def generate_answer(
+        vector_index: Any,
+        query: str,
+        chat_history: List[Tuple[str, Any]] = [],
+) -> Any:
+    # verbose=True - respuestas menos concisas
+    # temperature=0 - respuestas no creativas
+    chat = ChatOpenAI(verbose=True, temperature=0)
+
+    answer = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=vector_index.as_retriever()
+    )
+
+    return answer({"question": query, "chat_history": chat_history})
--- main.py
+++ main.py
@@ -6,11 +6,21 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
-from backend.core import indexing_pdf
+from backend.core import indexing_pdf, generate_answer
 
+
+load_dotenv()
 
 def main():
-    load_dotenv()
+    if "user_prompt_history" not in st.session_state:
+        st.session_state["user_prompt_history"] = []
+
+    if "chat_answer_history" not in st.session_state:
+        st.session_state["chat_answer_history"] = []
+
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
+
     st.set_page_config(page_title="PDF bot")
     st.header("PDF chatbot - Juan Miguel Sánchez Milla")
 
@@ -20,19 +30,41 @@ def main():
         # índice vectorial del texto del pdf. Usa FAISS
         vector_index = indexing_pdf(pdf)
 
-        # show user input
-        user_question = st.text_input("Ask a question about your PDF:")
-        if user_question:
+        # user input
+        user_input = st.text_input("Pregunta a tu PDF:")
+
+        if st.session_state["chat_answer_history"]:
+            for generated_response, user_query in zip(
+                st.session_state["chat_answer_history"],
+                st.session_state["user_prompt_history"]
+            ):
+                message(user_query, is_user=True)
+                message(generated_response)
+
+        if user_input:
+            with st.spinner("Generando respuesta..."):
+                generated_response = generate_response(
+                    vector_index,
+                    query=user_input,
+                    chat_history=st.session_state["chat_history"]
+                )
+
+                # añadir datos a la sesión de streamlit
+                st.session_state["user_prompt_history"].append(user_input)
+                st.session_state["chat_answer_history"].append(generated_response)
+                st.session_state["chat_history"].append((user_input, generated_response["answer"]))
+        # if user_question:
             # documentos(langchain) similares a user_question
-            docs = vector_index.similarity_search(user_question)
+            # docs = vector_index.similarity_search(user_question)
 
-            llm = OpenAI()
-            chain = load_qa_chain(llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=user_question)
-                print(cb)
+            # llm = OpenAI()
+            # chain = load_qa_chain(llm, chain_type="stuff")
+            # with get_openai_callback() as cb:
+            #     response = chain.run(input_documents=docs, question=user_question)
+            #     print(cb)
+            #
+            # st.write(response)
 
-            st.write(response)
 
 
 if __name__ == '__main__':
