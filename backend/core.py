ce42c95fc969d83eb34cf30ab3e262692eacda5c
Juan Miguel Sánchez Milla <juanmsmilla@gmail.com>
2023-05-31 15:07:32 +0200

    Component Error. Url to reactjs. Uploading it to find out more about it

--- backend/core.py
+++ backend/core.py
@@ -1,5 +1,5 @@
 from typing import Any, List, Tuple
-
+import streamlit as st
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -8,9 +8,19 @@ from langchain.text_splitter import CharacterTextSplitter
 from PyPDF2 import PdfReader
 
 
+# el decorador cachea el archivo
+
+# @st.experimental_memo(experimental_allow_widgets=True)
+@st.cache_data(experimental_allow_widgets=True)
+def get_pdf():
+    return st.file_uploader("Arrastra aquí tu PDF", type="pdf")
+
+
 def indexing_pdf(pdf, chunk_size: int = 1000, chunk_overlap: int = 200):
     # extraer texto
+    st.write("indexing_pdf")
     pdf_reader = PdfReader(pdf, )
+    st.write(type(pdf))
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
@@ -23,6 +33,7 @@ def indexing_pdf(pdf, chunk_size: int = 1000, chunk_overlap: int = 200):
         length_function=len
         )
     chunks = splitter.split_text(text)
+    st.write("chunk echos")
 
     # FAISS
     embeddings = OpenAIEmbeddings()
@@ -36,6 +47,7 @@ def generate_answer(
         query: str,
         chat_history: List[Tuple[str, Any]] = [],
 ) -> Any:
+    st.write("generate_answer")
     # verbose=True - respuestas menos concisas
     # temperature=0 - respuestas no creativas
     chat = ChatOpenAI(verbose=True, temperature=0)
@@ -43,5 +55,6 @@ def generate_answer(
     answer = ConversationalRetrievalChain.from_llm(
         llm=chat, retriever=vector_index.as_retriever()
     )
+    st.write(f"answer is {answer}")
 
     return answer({"question": query, "chat_history": chat_history})
--- main.py
+++ main.py
@@ -8,70 +8,74 @@ import streamlit as st
 # from langchain.callbacks import get_openai_callback
 from streamlit_chat import message
 
-from backend.core import indexing_pdf, generate_answer
-
 
 load_dotenv()
 
-def main():
-    if "user_prompt_history" not in st.session_state:
-        st.session_state["user_prompt_history"] = []
 
-    if "chat_answer_history" not in st.session_state:
-        st.session_state["chat_answer_history"] = []
+st.set_page_config(page_title="PDF bot")
+st.header("PDF chatbot - Juan Miguel Sánchez Milla")
 
-    if "chat_history" not in st.session_state:
-        st.session_state["chat_history"] = []
 
-    st.set_page_config(page_title="PDF bot")
-    st.header("PDF chatbot - Juan Miguel Sánchez Milla")
+from backend.core import indexing_pdf, generate_answer, get_pdf
 
-    if "pdf" not in st.session_state:
-        pdf = st.file_uploader("Adjuntar pdf", type="pdf")
-        st.session_state["pdf"] = pdf
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
 
-    pdf = st.session_state["pdf"]
+if "chat_answer_history" not in st.session_state:
+    st.session_state["chat_answer_history"] = []
 
-    if pdf is not None:
-        # índice vectorial del texto del pdf. Usa FAISS
-        vector_index = indexing_pdf(pdf)
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
-        # user input
-        user_input = st.text_input("Pregunta a tu PDF:")
 
-        if st.session_state["chat_answer_history"]:
-            for generated_response, user_query in zip(
+# pdf = st.sidebar.file_uploader("Arrastra un .pdf", type="pdf", accept_multiple_files=True)
+pdf = get_pdf()
+
+if pdf is not None:
+    # índice vectorial del texto del pdf. Usa FAISS
+    vector_index = indexing_pdf(pdf)
+
+    # user input
+    user_input = st.text_input("Pregunta a tu PDF:")
+
+
+
+    if user_input:
+        with st.spinner("Generando respuesta..."):
+            st.write("spinner")
+            generated_response = generate_answer(
+                vector_index,
+                query=user_input,
+                chat_history=st.session_state["chat_history"]
+            )
+
+            # añadir datos a la sesión de streamlit
+            st.session_state["user_prompt_history"].append(user_input)
+            st.session_state["chat_answer_history"].append(generated_response)
+            st.session_state["chat_history"].append((user_input, generated_response["answer"]))
+
+
+    st.write(st.session_state["chat_answer_history"])
+    if st.session_state["chat_answer_history"]:
+        st.write("pasa 1")
+        for generated_response, user_query in zip(
                 st.session_state["chat_answer_history"],
                 st.session_state["user_prompt_history"]
-            ):
-                message(user_query, is_user=True)
-                message(generated_response)
+        ):
+            st.write("pasa 2")
+            message(user_query, is_user=True)
+            message(generated_response)
+    # if user_question:
+        # documentos(langchain) similares a user_question
+        # docs = vector_index.similarity_search(user_question)
 
-        if user_input:
-            with st.spinner("Generando respuesta..."):
-                generated_response = generate_answer(
-                    vector_index,
-                    query=user_input,
-                    chat_history=st.session_state["chat_history"]
-                )
-
-                # añadir datos a la sesión de streamlit
-                st.session_state["user_prompt_history"].append(user_input)
-                st.session_state["chat_answer_history"].append(generated_response)
-                st.session_state["chat_history"].append((user_input, generated_response["answer"]))
-        # if user_question:
-            # documentos(langchain) similares a user_question
-            # docs = vector_index.similarity_search(user_question)
-
-            # llm = OpenAI()
-            # chain = load_qa_chain(llm, chain_type="stuff")
-            # with get_openai_callback() as cb:
-            #     response = chain.run(input_documents=docs, question=user_question)
-            #     print(cb)
-            #
-            # st.write(response)
+        # llm = OpenAI()
+        # chain = load_qa_chain(llm, chain_type="stuff")
+        # with get_openai_callback() as cb:
+        #     response = chain.run(input_documents=docs, question=user_question)
+        #     print(cb)
+        #
+        # st.write(response)
 
 
 
-if __name__ == '__main__':
-    main()
